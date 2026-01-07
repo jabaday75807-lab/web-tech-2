37b79d07b7cea3e8f9e9c47abd9455b7f8d583ea
Jethro Abaday <jabaday75807@liceo.edu.ph>
2026-01-07 13:55:21 +0800

    prelim submission

---
+++ src/app/activity/app.route.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { Activity1Component } from './activity-1.compnent';
+
+
+export const routes: Routes = [
+  { path: 'activity-1', component: Activity1Component }
+];
